dea422c44253cd8860fce90246227cd7a9c82670
kay-han <85919291+kay-han@users.noreply.github.com>
2021-06-28 19:21:45 -0500

    Applying ckeditor from Django
    Changed a simple text editor to rich text editor using ckeditor

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'teamblog',
     'membership',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
--- teamblog/models.py
+++ teamblog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime   import datetime, date
-
+from ckeditor.fields import RichTextField
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -17,7 +17,8 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete = models.CASCADE)
-    body = models.TextField()
+    body = RichTextField(blank=True, null=True)    #using ckeditor
+    #body = models.TextField()
     created_date = models.DateTimeField(auto_now_add = True)
     category = models.CharField(max_length=255, default='uncategorized')
 
