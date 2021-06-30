75573ea1c82f4ac3c1f0beccc0878ce19c2b00a0
kay-han <85919291+kay-han@users.noreply.github.com>
2021-06-29 20:19:50 -0500

    ChangePassword Function and Forms

--- membership/urls.py
+++ membership/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView
+from django.contrib.auth import views as auth_views  #It allows using some of the views that come with the authentication system comes with django
+from . import views
 
 urlpatterns = [
     path('registeration/', UserRegisterView.as_view(), name='registeration'),
     path('edit_profile/', UserEditView.as_view(), name='edit-profile'),    
+    #path('password/', auth_views.PasswordsChangeView.as_view(template_name='registration/change-password.html')),
+    path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html')),
+    path('password_success/', views.password_success, name='password_success.html'),
 ]
--- membership/views.py
+++ membership/views.py
@@ -1,19 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm, UserChangeForm
+from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
+from .forms import EditProfileForm, SignUpForm, PasswordChangingForm
 
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
+
 class UserEditView(generic.UpdateView):
-    form_class = UserChangeForm
+    form_class = EditProfileForm
     template_name = 'registration/edit_profile.html'
     success_url = reverse_lazy('home')
 
     def get_object(self):
         return self.request.user
 
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangingForm
+    #form_class = PasswordChangeForm
+    success_url = reverse_lazy('password_success')
+    #success_url = reverse_lazy('home')
 
+def password_success(request):
+    return render(request, 'registration/password_success.html', {})
